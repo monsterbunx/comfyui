17a192c8ee55372e52c1eef7f302f0202d1fc2e9
monsterbunx <hello@bunx.monster>
2026-04-02 04:37:14 -0600

    Instalando dependencias de ComfyUI-Manager

--- main.sh
+++ main.sh
@@ -98,7 +98,8 @@ uv pip install -r requirements.txt
 echo "Instalando dependencias de ComfyUI-Manager"
 uv pip install -r manager_requirements.txt
 
-
+echo "Instalando dependencias de ComfyUI-Manager"
+uv pip install -r custom_nodes/comfyui-manager/requirements.txt
 
 
 # Ejecutar en screen
